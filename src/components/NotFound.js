d5f7283523b81698a7b263fb7c512eb4cca55be0
Luis Rojas <luirojas3@crsjopgp0268.global.publicisgroupe.net>
2017-01-26 16:22:50 -0600

    Implementing React router

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//Stateles functional components
+
+const NotFound = (props) => {
+	return (
+	 	<h2>Not Found</h2>
+	)
+}
+
+export default NotFound; 
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
-	render() {
-		return <p>Hello</p>
+	// constructor(){
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+
+	goToStore(event) {
+		event.preventDefault();
+		//First grab the text from the box
+		const storedId = this.storeInput.value;
+		console.log(`Going to ${storedId}`);
+		//Second we're going to transition from / to /store/:storeId
+		this.context.router.transitionTo(`/store/${storedId}`);
 	}
+
+	render() {
+		return (
+			<form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+				{/* This is a JSX comment */} 
+				<h2>Please enter a store</h2>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
+				<button type="submit">Visit Store -> </button>
+			</form>
+		)
+	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
 }
 
 export default StorePicker;
